6c41069008b6a819d89a9898112f1f108e282a10
Enaya Jawed <jawedenaya@gmail.com>
2026-04-02 12:05:47 -0500

    Add main

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+package src;
+
+public class Main {
+    public static void main(String[] args) {
+        LogoutGUI logoutGUI = new LogoutGUI();
+
+        // user clicks logout
+        logoutGUI.clickLogout("user123");
+    }
+}
