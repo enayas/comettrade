9341513067dfa28b2c8c6e1ce38420f11f42500e
Enaya Jawed <jawedenaya@gmail.com>
2026-04-02 12:02:24 -0500

    Add AuthService and AuthController

---
+++ src/AuthService.java
@@ -0,0 +1,13 @@
+package src;
+
+public class AuthService {
+
+    public boolean logout(String userId) {
+        System.out.println("User logged out: " + userId);
+        return true;
+    }
+    public boolean login(String userId) {
+        System.out.println("User logged in: " + userId);
+        return true;
+    }
+}
--- src/User.java
+++ src/User.java
@@ -12,6 +12,7 @@ public class User {
     public String getUsername(){
         return username;
     }
+
     public String getPassword(){
         return password;
     }
