202f05e0d6c3cdfbc92b542dd18dfcab4ab94766
Enaya Jawed <jawedenaya@gmail.com>
2026-04-02 12:05:24 -0500

    Add LogoutGUI

---
+++ src/LogoutGUI.java
@@ -0,0 +1,14 @@
+package src;
+
+public class LogoutGUI {
+    private AuthController controller = new AuthController();
+
+    public void clickLogout(String userId) {
+        String msg = controller.logout(userId);
+        System.out.println(msg);
+
+        // redirect to login after cameron is done with it something like this:
+        // LoginGUI loginGUI = new LoginGUI();
+        // loginGUI.show();
+    }
+}
